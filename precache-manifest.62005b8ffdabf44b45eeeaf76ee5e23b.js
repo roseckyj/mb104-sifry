9e44f9cc0a7e6ee0aa6453f0cd2c367c916550f4
Jonáš Rosecký <rosecky.jonas@centrum.cz>
2020-06-12 15:51:22 +0200

    Updates

---
+++ precache-manifest.62005b8ffdabf44b45eeeaf76ee5e23b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfe8f6388fd06431b1cac075faeff59",
+    "url": "/mb104-sifry/index.html"
+  },
+  {
+    "revision": "471b5bc3c6cf9d07e416",
+    "url": "/mb104-sifry/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b951351b18cb99d27656",
+    "url": "/mb104-sifry/static/css/main.7b158b46.chunk.css"
+  },
+  {
+    "revision": "471b5bc3c6cf9d07e416",
+    "url": "/mb104-sifry/static/js/2.8cad08a3.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/mb104-sifry/static/js/2.8cad08a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b951351b18cb99d27656",
+    "url": "/mb104-sifry/static/js/main.1648ca8f.chunk.js"
+  },
+  {
+    "revision": "b023f941e8e7a9b4ada4",
+    "url": "/mb104-sifry/static/js/runtime-main.5bf4d128.js"
+  }
+]);
